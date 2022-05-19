b9ae773577968a1f030b76be6c386a408e50d938
HesamHosseini <hesihesi81.hosseini@gmail.com>
2022-05-20 00:10:24 +0430

    Login added to te abb usung class in JS

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,9 @@
+const input = document.getElementsByTagName("input")[0];
+const triggerInput = document.getElementById("triggerInput");
+const getLocation = document.getElementById("getDeviceLocation");
+const modal = document.getElementsByClassName("popUpWeatherContainer")[0];
+
+let weatherData;
 class WeatherApi {
   constructor() {}
 
@@ -8,7 +14,7 @@ class WeatherApi {
     return await city;
   }
 
-  static async fetchUserLocation() {
+  static fetchUserLocation() {
     const latLong = {};
     const successCallback = (position) => {
       const latitude = position.coords.latitude;
@@ -19,44 +25,138 @@ class WeatherApi {
     const errorCallback = (error) => {
       console.log(error);
     };
-    navigator.geolocation.getCurrentPosition(successCallback, errorCallback, {
-      enableHighAccuracy: true,
-    });
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
     return latLong;
   }
 
-  static async GeoLocationToCityApi(lat, long) {
+  static async fetchCordsCityName(lat, long) {
     const res = await fetch(
       `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${long}&key=362eae9e02344749b2ce47d063900456`
     );
     const locationInfo = await res.json();
-    return await locationInfo.results[0].components.city;
+    return locationInfo;
   }
 
-  static final() {
-    // const cords = WeatherApi.fetchUserLocation();
-    // console.log(cords);
-    // const mamad = WeatherApi.GeoLocationToCityApi(
-    //   cords.latitude,
-    //   cords.longitude
-    // );
-    // console.log(await mamad);
-    const a = WeatherApi.fetchUserLocation().then((cords) => {
-      return cords;
-    });
-    console.log(a);
+  static async fetchCityWeather(cityName) {
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4c76102d339696a4f37636f9ad7219ba&units=metric`
+    );
+    const weather = await res.json();
+    return await weather;
+  }
+
+  static modalPainter(weatherLog) {
+    modal.innerHTML = `
+  <div class="modalHeader d-flex flex-row mx-3">
+              <div class="returnBtn pt-2">
+                <img src="./assets/img/return.svg" alt="return" />
+              </div>
+              <h1 class="px-5 py-2">weather App</h1>
+            </div>
+            <div class="d-flex flex-column">
+              <div class="weatherImgContainer text-center">
+                <img
+                  src="https://openweathermap.org/img/wn/${weatherLog.weather[0].icon}@2x.png"
+                  alt="weatherImg"
+                />
+              </div>
+              <div class="weatherTemp text-center text-capitalize">
+                <span>${weatherLog.main.temp}</span>°C<span></span>
+              </div>
+              <div class="weatherSummery text-center fs-2">
+                ${weatherLog.weather[0].description}
+              </div>
+              <div class="weatherLocation text-center pb-5">
+                <span>
+                  <img src="./assets/img/location.svg" alt="location" />
+                </span>
+                <span>${weatherLog.name} </span> ,
+                <span>${weatherLog.sys.country}</span>
+              </div>
+              <div class="row text-center">
+                <div class="col">
+                  <div
+                    class="d-flex p-3 flex-row justify-content-center align-items-center"
+                    style="border-right: 0.5px solid gray"
+                  >
+                    <div>
+                      <img
+                        class="humidityAndtemretureIcon"
+                        src="./assets/img/tempreture.svg"
+                        alt="tempreture"
+                      />
+                    </div>
+                    <div class="d-flex flex-column">
+                      <div class="fs-2"><span>${weatherLog.main.feels_like}</span>°C<span></span></div>
+                      <div>Feels like</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div
+                    class="d-flex p-3 flex-row justify-content-center align-items-center"
+                    style="border-right: 0.5px solid gray"
+                  >
+                    <div>
+                      <img
+                        class="humidityAndtemretureIcon"
+                        src="./assets/img/humidity.svg"
+                        alt="humidity"
+                      />
+                    </div>
+                    <div class="d-flex flex-column">
+                      <div class="fs-2"><span>${weatherLog.main.humidity}</span>%<span></span></div>
+                      <div>Humidity</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>`;
+    const backBtn = document.getElementsByClassName("returnBtn")[0];
+    backBtn.addEventListener("click", WeatherApi.reset);
+    modal.classList.add("animation");
+    input.value = "";
+  }
+  static reset() {
+    modal.innerHTML = "";
+    modal.classList.remove("animation");
   }
 }
 
-// function clgFunc() {
-//   console.log("trigered");
-// }
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    triggerInput.click();
+  }
+});
+const cords = WeatherApi.fetchUserLocation();
 
-// const a = document.getElementsByTagName("input")[0];
-// console.log(a);
-// a.addEventListener("keypress", function (event) {
-//   if (event.key === "Enter") {
-//     event.preventDefault();
-//     document.getElementById("triggerInput").click();
-//   }
-// });
+const weatherGetter = async () => {
+  const cityName = input.value;
+  const weather = await WeatherApi.fetchCityWeather(cityName);
+  console.log(weather);
+  WeatherApi.modalPainter(weather);
+};
+const chromeOrNot = async () => {
+  if (navigator.userAgent.match(/chrome|chromium|crios/i)) {
+    // debugger;
+    const cityName = await WeatherApi.fetchAPI_IP();
+    const weather = await WeatherApi.fetchCityWeather(cityName.city);
+    console.log(await weather);
+    WeatherApi.modalPainter(weather);
+  } else {
+    console.log(cords);
+
+    const locationInfo = await WeatherApi.fetchCordsCityName(
+      cords.latitude,
+      cords.longitude
+    );
+    const address = await locationInfo.results[0].formatted;
+    const cityName = address.split(",")[0];
+    const weather = await WeatherApi.fetchCityWeather(cityName);
+    console.log(await weather);
+    WeatherApi.modalPainter(weather);
+  }
+};
+triggerInput.addEventListener("click", weatherGetter);
+getLocation.addEventListener("click", chromeOrNot);
